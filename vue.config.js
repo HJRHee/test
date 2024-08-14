bf8888b5031697e55d2a26e2691f3c8727a4c821
min-00 <rldkatjd3579@naver.com>
2024-08-14 18:10:12 +0900

    'test777'

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
 transpileDependencies: true,
 outputDir: './docs',
-publicPath: process.env.NODE_ENV === 'test'
+publicPath: process.env.NODE_ENV === 'production'
 ? '/test/'
 : '/'
 })
